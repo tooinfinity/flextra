efa7855c46643afcc7e94c4d51748cb0d2312c89
TouwfiQ Meghlaoui <touwfiqdev@gmail.com>
2025-01-24 00:35:11 +0100

    chore:fix sveltekit warning

--- stubs/inertia-svelte/resources/js/Layouts/AuthenticatedLayout.svelte
+++ stubs/inertia-svelte/resources/js/Layouts/AuthenticatedLayout.svelte
@@ -74,6 +74,7 @@
                     <!-- Hamburger -->
                     <div class="-me-2 flex items-center sm:hidden">
                         <button
+                            aria-labelledby="Show navigation"
                             onclick={() => (showingNavigationDropdown = !showingNavigationDropdown)}
                             class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none dark:text-gray-500 dark:hover:bg-gray-900 dark:hover:text-gray-400 dark:focus:bg-gray-900 dark:focus:text-gray-400"
                         >
