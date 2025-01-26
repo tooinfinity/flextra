ba6e970139cc9bbcfdeb4834290e96b31d56a0c7
TouwfiQ Meghlaoui <touwfiqdev@gmail.com>
2025-01-26 22:53:21 +0100

    fix:Could not resolve entry module Modules/*/resources/js/app.js

--- stubs/blade-module/vite.config.js
+++ stubs/blade-module/vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'Modules/*/resources/js/app.js', 'Modules/*/resources/css/app.css'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'Modules/Auth/resources/js/app.js'],
             refresh: true,
         }),
     ],
