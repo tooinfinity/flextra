d78bdd16851260783e9e58b6432c20999770c3e4
TouwfiQ Meghlaoui <touwfiqdev@gmail.com>
2025-01-28 23:11:11 +0100

    fix:tests fails react

--- stubs/inertia-react/vite.config.js
+++ stubs/inertia-react/vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': '/resources/js',
-            '@auth': '/Modules/Auth/resources/assets/js'
+            '@auth': '/Modules/{{moduleName}}/resources/assets/js'
         }
     },
     optimizeDeps: {
